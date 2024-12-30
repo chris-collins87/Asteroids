58dff9026268744b4e9bce0100dc1961406df42d
chris-collins87 <christopher.todd.collins@gmail.com>
2024-12-29 20:46:08 -0600

    Updated main game

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def main():
     print("Starting asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -13,6 +17,9 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+        
+        
+        dt = clock.tick(60) / 1000
 
 
 
